bcd241c2b22a7b32df0da03fcfe44ad43259e9ae
Prashans Dixit <71697179+prishuprograms@users.noreply.github.com>
2020-10-01 13:02:34 +0530

    Updating Code by adding comments

--- alphabet_pattern.py
+++ alphabet_pattern.py
@@ -1,8 +1,12 @@
+# Counter
 count = 1
+
+# running for loop
 for i in range(1,7):
     A=65
     for j in range(0,count):
         print(chr(A),end='')
         A = A + 1
+    # Printing results
     print()
     count = count + 1
